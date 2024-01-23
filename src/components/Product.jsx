0eb6bfc27291ba50ef55b176bd1be862a40e7c79
Fritz <fritz.developer10@gmail.com>
2024-01-22 21:09:55 -0500

    segundo commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,10 +7,10 @@ const Product = () => {
             <img className="product__img" src="https://picsum.photos/180" alt="product" />
             <h2 className="product__name">camisa</h2>
             <div className="product__details">
-                <p className="product__old">S/ 200</p>
+                <p className="product__oldPrice">S/ 200</p>
                 <p className="product__price">S/ 100</p>
+                <AddCartBtn/>
             </div>
-            <AddCartBtn/>
     </div>
   )
 }
